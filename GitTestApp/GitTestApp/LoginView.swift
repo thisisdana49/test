4c6f967de643dfc43ca74d505323bd33fced5547
조다은 <chodaeun@Danibook.local>
2024-06-27 22:57:52 +0900

    [Feat] 로그인 버튼 구현

---
+++ GitTestApp/GitTestApp/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  GitTestApp
+//
+//  Created by 조다은 on 6/27/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
